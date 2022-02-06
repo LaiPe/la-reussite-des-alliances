922e89c2eef16bd721f85201edddbe5bfe291ca4
Gabriel Caux <gabriel.caux@gmail.com>
2022-02-06 15:47:38 +0100

    init pioche alea

--- prog.py
+++ prog.py
@@ -1,3 +1,4 @@
+import random
 #=========AFFICHAGE=============
 def carte_to_chaine(carte):
     dico=dict(carte)
@@ -37,11 +38,28 @@ def ecrire_fichier_reussite(nomFichier,liCartes):
     for carte in liCartes:
         f.write(carte["valeur"]+"-"+carte["couleur"]+" ")
     f.close()
+def init_pioche_alea(nb_cartes = 32):
+    liCartes = []
+    carte = {}
+    couleurs = ['C','P','K','T']
+    num_valeur32 = ['7','8','9','10','A','V','D','R']
+    valeurs = ['2','3','4','5','6','7','8','9','10','A','V','D','R']
+    if nb_cartes == 32:
+        valeurs = num_valeur32
+    for couleur in couleurs:
+        for valeur in valeurs:
+            carte['valeur']=valeur
+            carte['couleur']=couleur
+            liCartes += [dict(carte)]   
+    random.shuffle(liCartes)
+    return liCartes
+        
+    
 if __name__ == "__main__":
     print("Bonjour")
     carte={"valeur":3,"couleur":"C"}
     liCartes=[{"valeur":3,"couleur":"C"},{"valeur":10, "couleur":"K"},{"valeur":9,"couleur":"T"}]
-
+    print(chr(9825))
     #afficher_reussite(liCartes)
     #print(carte_to_chaine(carte))
     jeu=init_pioche_fichier("data_init.txt")
@@ -49,3 +67,5 @@ if __name__ == "__main__":
     ecrire_fichier_reussite("test.txt",jeu)
     jeu=init_pioche_fichier("test.txt")
     afficher_reussite(jeu)
+    jeu2 = init_pioche_alea()
+    afficher_reussite(jeu2)
